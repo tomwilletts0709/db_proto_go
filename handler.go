99da76cd6e035206cab0577f73187e69a0aa2fef
Ashleigh Hewitt <ashleighhewitt@Ashleighs-MacBook-Pro-2.local>
2026-02-07 08:26:00 +0000

    update handler

--- handler.go
+++ handler.go
@@ -1,1 +1,100 @@
-package goredisrebuildproject
+package main
+
+import (
+	"sync"
+)
+
+var Handlers = map[string]func([] Value) Value{
+	"PING": ping, 
+	"SET": set,
+	"GET": get,
+	"HSET": hset,
+	"HGET": hget,
+	"HGETALL": hgetall,
+}
+
+func ping(args []Value) Value {
+	if len(args) == 0 {
+		return Value{typ: "string", str: "PONG"}
+	}
+	return Value{typ: "string", str: args[0].str}
+}
+
+var SETs = map[string]Value{}
+var SETsMu = sync.RWMutex{}
+
+func set(args []Value) Value {
+	if len(args) != 2 {
+		return Value{typ: "error", str: "ERR wrong number of arguments for 'set' command"}
+	}
+
+	key := args[0].bulk
+	value := args[1].bulk
+
+	SETsMu.Lock()
+	SETs[key] = Value{typ: "string", str: value}
+	SETsMu.Unlock()
+
+	return Value{typ: "string", str: "OK"}
+}
+
+func get(args []Value) Value {
+	if len(args) != 1 {
+		return Value{typ: "error", str: "ERR wrong number of arguments for 'get' command"}
+	}
+
+	key := args[0].bulk
+	SETsMu.RLock()
+	value, ok := SETs[key]
+	SETsMu.RUnlock()
+
+	if !ok {
+		return Value{typ: "nil"}
+	}
+
+	return value{typ: "bulk", bulk: value}
+
+}
+
+var HSETs = map[string]map[string]string{}
+var HSETsMu = sync.RWMutex{}
+
+func hset(args []Value) Value {
+	if len(args) != 3 {
+		return Value{typ: "error", str: "ERR wrong number of arguments for 'hset' command"}
+	}
+
+	hash := args[0].bulk
+	key := args[1].bulk
+	value := args[2].bulk
+
+	HSETsMu.Lock()
+
+	if _, ok := HSETs[hash]; !ok {
+		HSETs[hash] = map[string]string{}
+	}
+
+	HSETs[hash][key] = value
+
+	HSETsMu.Unlock()
+
+	return Value(typ: "string", str: "OK")
+}
+
+func hget(args []Value) Value {
+	if len(args) != 2 {
+		return Value(typ: "error", str: "ERR wrong number of arguments for 'hget' command")
+	}
+
+	hash := args[0].bulk
+	key := args[1].bulk
+
+	HSETsMu.RLock()
+	value, ok := HSETs[hash][key]
+	HSETsMu.RUnlock()
+
+	if !ok {
+		return Value{typ: "nil"}
+	}
+	return Value{typ: "bulk", bulk: value}
+}
--- resp.go
+++ resp.go
@@ -1,5 +1,11 @@
+package main
 
-
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"strconv"
+)
 
 const (
 	STRING = '+'
@@ -23,7 +29,7 @@ type Resp struct {
 }
 
 func NewResp (rd io.Reader) *Resp {
-	return &Rresp{reader: bufio.NewReader(rd)}
+	return &Resp{reader: bufio.NewReader(rd)}
 }
 
 func (r *Resp) readLine() (line []byte, n int, err error) {
@@ -35,7 +41,7 @@ func (r *Resp) readLine() (line []byte, n int, err error) {
 
 		n += 1 
 		line = append(line, b)
-		if len(line) >= 2 && line[len(line)-2] == \'r' {
+		if len(line) >= 2 && line[len(line)-2] == '\r' {
 			break
 		}
 	}
@@ -112,4 +118,96 @@ func (r *Resp) readBulk() (Value, error) {
 	r.readLine()
 
 	return v, nil
+}
+
+func (v Value) Marshal() []byte {
+	switch v.typ {
+	case "array":
+		return v.marshalArray()
+	case "bulk":
+		return v.marshalBulk()
+	case "string":
+		return v.marshalString()
+	case "error":
+		return v.marshalError()
+	case "nil":
+		return v.marshalNil()
+	default:
+		return []byte{}
+	}
+}
+
+func (v Value) marshalString() []byte {
+	var bytes []byte
+	bytes = append(bytes, STRING)
+	butes = append(bytes, v.str...)
+	bytes = append(bytes, '\r', '\n')
+	
+	return bytes
+}
+
+func (v Value) marshalBulk() []byte {
+	var
+	bytes = append(bytes, BULK)
+	bytes = append(bytes, strconv.Itoa(len(v.bulk))...)
+	bytes = append(bytes, '\r', '\n')
+	bytes = append(bytes, v.bulk...)
+	bytes = append(bytes, '\r', '\n')
+	return bytes
+}
+
+func (v Value) marshalError() []byte {
+	var bytes []byte
+	bytes = append(bytes, ERROR)
+	bytes = append(bytes, v.str...)
+	bytes = append(bytes, '\r', '\n')
+	return bytes
+}
+
+func (v Value) marshalArray() []byte {
+	len := len(v.array)
+	var bytes []byte
+	bytes = append(bytes, ARRAY)
+	bytes = append(bytes, strconv.Itoa(len)...)
+	bytes = append(bytes, '\r', '\n')
+
+	for i := 0; i < len; i++ {
+		bytes = append(bytes, v.array[i].Marshal()...)
+	}
+
+	return bytes
+}
+
+func (v Value) marshalError() []byte {
+	var bytes []byte 
+	bytes = append(bytes, ERROR)
+	bytes = append(bytes, v.str...)
+	bytes = append(bytes, '\r', '\n')
+	return bytes
+}
+
+func (v Value) marshalNil() []byte {
+	return []byte("$-1\r\n")§
+}
+
+
+//writer
+
+type Writer struct {
+	writer io.Writer
+}
+
+func NewWriter(w io.Writer) *Writer {
+	return &Writer{writer: w}
+}
+
+func (w *Writer) Write(value Value) error {
+	var bytes = value.Marshal()
+
+	_, err := w.writer.Write(bytes)
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
